2571bcf8c591867f1304afb9277820a57ac68a11
Sergey Zaborovsky <git@seryiza.xyz>
2022-01-18 21:40:59 +1100

    Add links table to migration and queries.sql

---
+++ resources/migrations/20220117133657-add-links-table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE links (
+  id SERIAL PRIMARY KEY,
+  url TEXT,
+  emojis TEXT
+);
--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,14 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+-- :name get-links :? :*
+-- :doc Get all links
+SELECT * FROM links;
 
--- :name update-user! :! :n
--- :doc updates an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
+-- :name get-link :? :1
+-- :doc Get the link
+SELECT * FROM links
+WHERE id = :id;
 
--- :name get-user :? :1
--- :doc retrieves a user record given the id
-SELECT * FROM users
-WHERE id = :id
-
--- :name delete-user! :! :n
--- :doc deletes a user record given the id
-DELETE FROM users
-WHERE id = :id
+-- :name create-link! :! :n
+-- :doc Add a new link
+INSERT INTO links
+(url, emojis)
+VALUES (:url, :emojis)
