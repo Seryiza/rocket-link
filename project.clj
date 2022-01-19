4ae0e64eafd871446af1182aba8bb86b32713a9a
Sergey Zaborovsky <git@seryiza.xyz>
2022-01-19 20:09:03 +1100

    Add first migration with Migratus + next.jdbc

--- project.clj
+++ project.clj
@@ -5,8 +5,15 @@
                  [ring/ring-jetty-adapter "1.9.5"]
                  [ring/ring-mock "0.4.0"]
                  [metosin/reitit-ring "0.5.15"]
-                 [mount "0.1.16"]]
+                 [mount "0.1.16"]
+                 [migratus "1.3.5"]
+                 [com.github.seancorfield/next.jdbc "1.2.761"]
+                 [org.postgresql/postgresql "42.3.1"]]
   :ring {:init rocket-link.app/init-app
          :handler rocket-link.app/app
          :nrepl {:start? true :port 7000}}
-  :plugins [[lein-ring "0.12.6"]])
+  :migratus {:store :database
+             :migration-dir "migrations"
+             :db ~(-> "config.edn" slurp clojure.edn/read-string :database-url)}
+  :plugins [[lein-ring "0.12.6"]
+            [migratus-lein "0.7.3"]])
