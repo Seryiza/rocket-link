5c342d3982926ab84950239c502f6f0e7891969b
Sergey Zaborovsky <git@seryiza.xyz>
2022-01-21 01:04:30 +1100

    Add creating jdbc opts from database-url (Dokku)

--- src/rocket_link/db.clj
+++ src/rocket_link/db.clj
@@ -1,12 +1,26 @@
 (ns rocket-link.db
   (:require [mount.core :refer [defstate]]
             [hikari-cp.core :as cp]
-            [rocket-link.config :refer [config]]))
+            [rocket-link.config :refer [config]]
+            [clojure.string :as string]))
+
+(defn get-jdbc-opts [database-url]
+  (let [uri    (java.net.URI. database-url)
+        path   (.getPath uri)
+        host   (.getHost uri)
+        port   (.getPort uri)
+        [_ db] (re-find #"\/(.+)" path)
+        [username password] (string/split (.getUserInfo uri) #"\:")]
+    {:adapter "postgresql"
+     :server-name host
+     :port-number port
+     :database-name db
+     :username username
+     :password password}))
 
 (defstate db
   :start (let [{database-url :database-url} config
-               pool-opts {:jdbc-url database-url}
-               a (print database-url)
+               pool-opts (get-jdbc-opts database-url)
                datasource (cp/make-datasource pool-opts)]
            {:datasource datasource})
   :stop (-> db :datasource cp/close-datasource))
