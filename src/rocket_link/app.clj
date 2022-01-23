abfcde405b2e06d84f3760cfe855f6644b6e1246
Sergey Zaborovsky <git@seryiza.xyz>
2022-01-23 20:59:01 +1100

    Improve showing of emoji links on created page

--- src/rocket_link/app.clj
+++ src/rocket_link/app.clj
@@ -3,21 +3,19 @@
             [rocket-link.links.links :refer [get-link-by-code-name create-link!]]
             [rocket-link.html :refer [render]]
             [rocket-link.emoji.emoji-generator :refer [generate-emojis-by-id]]
+            [rocket-link.punycode :as punycode]
             [mount.core :as mount :refer [defstate]]
             [reitit.ring :as ring]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.util.response :refer [redirect]])
-  (:import [java.net URLEncoder IDN]))
+  (:import [java.net URLEncoder]))
 
 (defn url-encode [text]
   (URLEncoder/encode text "UTF-8"))
 
-(defn punycode-encode [text]
-  (IDN/toASCII text IDN/ALLOW_UNASSIGNED))
-
 (defn make-project-url [& components]
-  (apply str "https://" (-> config :base-domain punycode-encode) components))
+  (apply str "https://" (:base-domain config) components))
 
 (defn index-page-handler [request]
   (render request "index.html"))
@@ -26,7 +24,7 @@
   (let [url (-> request :params :url)
         created-shortcut (create-link! url generate-emojis-by-id)
         created-link-url (make-project-url "/links/" (url-encode created-shortcut) "/created")]
-    (redirect created-link-url)))
+    (punycode/redirect created-link-url)))
 
 (defn show-created-link-handler [request]
   (let [code-name (-> request :path-params :code-name)
@@ -38,7 +36,7 @@
         link (get-link-by-code-name code-name)]
     (if (nil? link)
       {:status 404, :body "Oh, non-existing URL"}
-      (redirect (:url link)))))
+      (punycode/redirect (:url link)))))
 
 (defstate app
   :start (ring/ring-handler
